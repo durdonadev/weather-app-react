b0bc2bff901da0f1a732f3c455b17c8caf36911c
Durdona Ibragimova <durdona.dev@gmail.com>
2023-12-22 01:08:57 -0500

    Create basic elements of weather component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import WeatherApp from "./Components/WeatherApp/WeatherApp";
 
 function App() {
-    return <div className="App"></div>;
+    return (
+        <div className="App">
+            <WeatherApp />
+        </div>
+    );
 }
 
 export default App;
